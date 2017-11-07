a919eda244b28bc94b59acd380c62e7c949bc335
leoShih <leo201325@gmail.com>
2017-11-07 21:54:17 +0800

    done

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -9,10 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var startNumber = 240
+    var timer = Timer()
+    var isTimeRunning = true
+    let t = 10
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBAction func playButton(_ sender: Any) {
+        
+       runTime()
+        
+    }
+    
+   
+    @IBAction func stopButton(_ sender: Any) {
+        
+        timer.invalidate()  //stop running time
+        
+    }
+   
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+        numberLabel.text = String(240)
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func plusTimeButton(_ sender: Any) {
+        
+        startNumber += 10
+        numberLabel.text = String(startNumber)
+       
+    }
+    
+    @IBAction func subTimeButton(_ sender: Any) {
+        
+        if startNumber >= 10 {
+        
+            startNumber -= 10
+            numberLabel.text = String(startNumber)
+        
+        }
+    }
+    
+    @objc func processTimer() {
+        
+        if startNumber > 0 {
+        
+            startNumber -= 1
+        numberLabel.text = "\(startNumber)"
+        
+        }else{
+            
+            timer.invalidate()
+            
+        }
+    }
+    
+    func runTime() {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    
+        numberLabel.text = String(startNumber)
+    
     }
 
     override func didReceiveMemoryWarning() {
