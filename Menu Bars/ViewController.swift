bfea4e27b8b34b3774df24ac7e8a5fabf64b7515
leoShih <leo201325@gmail.com>
2017-11-09 22:12:28 +0800

    final

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -12,27 +12,26 @@ class ViewController: UIViewController {
     
     var startNumber = 240
     var timer = Timer()
-    var isTimeRunning = true
-    let t = 10
     
     @IBOutlet weak var numberLabel: UILabel!
     @IBAction func playButton(_ sender: Any) {
         
-       runTime()
+        runTime()
         
     }
     
-   
+    
     @IBAction func stopButton(_ sender: Any) {
         
         timer.invalidate()  //stop running time
         
     }
-   
+    
     
     @IBAction func resetButton(_ sender: Any) {
         
-        numberLabel.text = String(240)
+        startNumber = 240
+        numberLabel.text = String(startNumber)
         timer.invalidate()
         
     }
@@ -41,54 +40,47 @@ class ViewController: UIViewController {
         
         startNumber += 10
         numberLabel.text = String(startNumber)
-       
+        
     }
     
     @IBAction func subTimeButton(_ sender: Any) {
         
         if startNumber >= 10 {
-        
+            
             startNumber -= 10
             numberLabel.text = String(startNumber)
-        
+            
         }
     }
     
-    @objc func processTimer() {
-        
-        if startNumber > 0 {
-        
-            startNumber -= 1
-        numberLabel.text = "\(startNumber)"
-        
-        }else{
-            
-            timer.invalidate()
-            
-        }
-    }
     
     func runTime() {
-        
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.tTimer)), userInfo: nil, repeats: true)
+    }
     
+    @objc func tTimer() {
+        if startNumber > 0 {
+            startNumber -= 1
+            numberLabel.text = String(startNumber)
+        }else{
+            timer.invalidate()
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
         
-    
         numberLabel.text = String(startNumber)
-    
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
